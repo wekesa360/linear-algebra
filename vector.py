283a3896c4a701fd612818765581eef23ce1a15e
Michael Wekesa <wekesa360@outlook.com>
2021-12-30 16:51:56 +0300

    Development: add methods for vector addition, subtraction and times scalar

--- vector.py
+++ vector.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 class Vector():
-	coordinates = (0, 0)
 	def __init__(self, coordinates):
 		try:
 			if not coordinates:
@@ -14,6 +13,19 @@ class Vector():
 			raise TypeError('The coordiantes must be an iterable')
 
 
+	def plus(self, v):
+		new_coordinates = [x+y for x,y in zip(self.coordinates, v.coordinates)]
+		return Vector(new_coordinates)
+
+	def minus(self, v):
+		new_coordinates = [x-y for x,y in zip(self.coordinates, v.coordinates)]
+		return Vector(new_coordinates)
+
+	def times_scalar(self, c):
+		new_coordinates = [c*x for x in self.coordinates]
+		return Vector(new_coordinates)
+	
+
 	def __str__(self):
 		return 'Vector: {}'.format(self.coordinates)
 
@@ -21,11 +33,13 @@ class Vector():
 	def __eq__(self, v):
 		return self.coordinates == v.coordinates
 
-#creating coordinates 
+
+
+#creating vectors
 my_vector = Vector([2,3,4])
 print (my_vector)
 
-#comparing cooordinates
+#comparing vectors
 my_vector1 = Vector([1,2,3])
 my_vector2 = Vector([1,2,3])
 print (my_vector1 == my_vector2)
@@ -34,3 +48,12 @@ print (my_vector1 == my_vector2)
 my_vector1 = Vector([1,3,3])
 my_vector2 = Vector([-1,1,3])
 print (my_vector1 == my_vector2)
+
+#addition of vectors 
+print (Vector.plus(my_vector1,my_vector2))
+
+#subtraction of vectors 
+print (Vector.minus(my_vector1,my_vector2))
+
+#times scaler of vectors 
+print (Vector.times_scalar(my_vector1, 2))
